cc616159cd6a758a71f91c5bb0eb3fd8f8cf1080
Najd Eriahi <najdriyehi@gmail.com>
2021-05-29 03:26:03 +0200

    style: language configuration and style update

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -62,7 +62,7 @@
             <div class="flex">
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                    <x-jet-nav-link href="{{ route('clients.index') }}" :active="request()->routeIs('clients.index')">
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
                 </div>
@@ -101,7 +101,7 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+            <x-jet-responsive-nav-link href="{{ route('clients.index') }}" :active="request()->routeIs('clients.index')">
                 {{ __('Dashboard') }}
             </x-jet-responsive-nav-link>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,9 @@ Route::get('/dashboard', function () {
 	return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-Route::get("/clients", [\App\Http\Controllers\CustomerController::class, "index"])->name("clients.index");
-Route::get("/client/{client}", [\App\Http\Controllers\CustomerController::class, "show"])->name("clients.show");
-Route::get("/client/{client}/invoice", [\App\Http\Controllers\ClientInvoiceController::class, 'show'])->name("clients.invoices.show");
+Route::middleware(['auth'])->get("/clients", [\App\Http\Controllers\CustomerController::class, "index"])->name("clients.index");
+Route::middleware(['auth'])->get("/client/{client}", [\App\Http\Controllers\CustomerController::class, "show"])->name("clients.show");
+Route::middleware(['auth'])->get("/client/{client}/invoice", [\App\Http\Controllers\ClientInvoiceController::class, 'show'])->name("clients.invoices.show");
 //Route::view("/client", "clients.show")->name("clients.show");
 
 
